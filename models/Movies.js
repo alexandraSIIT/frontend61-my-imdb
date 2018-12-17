dc5ab1729f4ad66e3797e0338469a01cdf450c30
raluchiuzbaian <raluchiuzbaian@yahoo.com>
2018-12-17 20:40:00 +0200

    adding pagination

--- models/Movies.js
+++ models/Movies.js
@@ -1,10 +1,12 @@
 function Movies() {
   this.items = [];
+  this.pagination = {};
 }
-// const moviesRootUrl = "https://ancient-caverns-16784.herokuapp.com/";
-Movies.prototype.getAll = function() {
+var moviesRootUrl = "https://ancient-caverns-16784.herokuapp.com/";
+Movies.prototype.getAll = function(options="0") {
   var me = this;
-  return $.get(moviesRootUrl + "movies?take=10&skip=0").then(function(response) {
+  return $.get(moviesRootUrl + "movies?take=10&skip=" + options).then(function(response) {
+  me.pagination=response.pagination;
   response=response.results;
   for (var i = 0; i < response.length; i++) {
     var movie = new Movie(response[i]);
